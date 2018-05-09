468db9f55f0d8d7f392400a999d3944edb5c763b
vita <dragic.hsieh@gmail.com>
2018-05-09 16:23:15 +0800

    Done add category index page for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,12 @@
 class Admin::CategoriesController < ApplicationController
 
-# 驗證請求進入後台的是否為已登入的 User
-before_action :authenticate_user!
-# 驗證該 User 身份是否為網站管理員
-before_action :authenticate_admin
+	# 驗證請求進入後台的是否為已登入的 User
+	before_action :authenticate_user!
+	# 驗證該 User 身份是否為網站管理員
+	before_action :authenticate_admin
+
+	def index
+		@categories = Category.all
+	end
 
 end
