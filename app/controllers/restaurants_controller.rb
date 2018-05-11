04b72fa2fbc2aa0e09416e1dade67b356dc5aac2
vita <dragic.hsieh@gmail.com>
2018-05-12 01:10:04 +0800

    Done user can link to dashboard from restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,32 @@
 class RestaurantsController < ApplicationController
 
+	before_action :set_restaurant, only: [:show, :dashboard]
+
 	def index
 		@restaurants = Restaurant.page(params[:page]).per(9)
 		@categories = Category.all
 	end
 
 	def show
-		@restaurant = Restaurant.find(params[:id])
 		@comment = Comment.new
 	end
 
 	# GET restaurants/feeds
-   	# 會去 render app/views/restuarants/feeds.html.erb
-   	def feeds
-     	@recent_restaurants = Restaurant.all.order(created_at: :desc).limit(10)
-     	@recent_comments = Comment.all.order(created_at: :desc).limit(10)
-   	end
+ 	# 會去 render app/views/restuarants/feeds.html.erb
+ 	def feeds
+   	@recent_restaurants = Restaurant.all.order(created_at: :desc).limit(10)
+   	@recent_comments = Comment.all.order(created_at: :desc).limit(10)
+ 	end
+
+ 	# GET restaurants/dashboard
+ 	# 會去 render app/views/restuarants/dashboard.html.erb
+ 	def dashboard
+ 	end
+
+ 	private
+
+ 	def set_restaurant
+ 		@restaurant = Restaurant.find(params[:id])
+ 	end
 	
 end
